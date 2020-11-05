b2490c3d20b0dbf92148cc9faef81d7858d16896
AlexanderLukashuk <upiter465@gmail.com>
2020-11-05 09:33:14 +0600

    Создан класс сущности User, сделано окно регистрации, процесс регистрации по нажатию кнопки

---
+++ RegistrationWithThread/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegistrationWithThread.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Password { get; set; }
+
+        public User(string login, string email, string phone, string password)
+        {
+            Login = login;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+    }
+}
