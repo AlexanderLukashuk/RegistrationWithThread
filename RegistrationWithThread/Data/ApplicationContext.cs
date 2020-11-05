d69620a2c3fc25e815c11648ce16676e4d5a931b
AlexanderLukashuk <upiter465@gmail.com>
2020-11-05 10:14:53 +0600

    Добавлены контекст и миграции

---
+++ RegistrationWithThread/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using RegistrationWithThread.Models;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistrationWithThread.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = UsersDb; Trusted_Connection = true");
+
+        }
+    }
+}
--- RegistrationWithThread/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ RegistrationWithThread/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -96,7 +96,7 @@ namespace RegistrationWithThread {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/RegistrationWithThread;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/RegistrationWithThread;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
